58f71299a901d4c1fa2a94f58d69fff3cbfb4dd5
Ken <flh070147000303@gmail.com>
2023-03-04 01:27:24 +0900

    新建自己的入口文件和组件

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//引入react核心库
+import React  from "react";
+//引入reactDOM
+
+//引入app组件
 import App from './App';
-//用于记录性能
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//渲染app组件到页面
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

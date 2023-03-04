42f23bae4b835f8854565c0c5d4afa0d4f120db2
Ken <flh070147000303@gmail.com>
2023-03-04 15:03:18 +0900

    用脚手架创建hello和welcome组件,关于component的分别暴露

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 //创建外壳组件
+//{Component} 不是结构赋值,是在react文件中利用了分别暴露
 import React ,{Component} from "react";
-class App extends Component{
+import Hello  from "./components/Hello/Hello";
+import Welcome from "./components/Welcome/Welcome";
+//创建并暴露app组件
+export default  class App extends Component{
     render(){
          return (
 
 
             <div>
-                hello.react!
+                <Hello />
+                <Welcome/>
             </div>
          )
     }
 }
 
 //暴露APP组件
-export default App
+// export default App
---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,11 @@
+import React ,{Component} from "react";
+import './Hello.css'
+export default class Hello extends Component{
+    render(){
+        return(
+            <div>
+                <h2 className="title">HELLO ,REACT</h2>
+            </div>
+        )
+    }
+}

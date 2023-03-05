944ae1f94138bcbf5c9eba13388429e83f5da7a3
Ken <flh070147000303@gmail.com>
2023-03-05 15:17:13 +0900

    配置proxy解决跨域问题,3000有的就请求3000的,3000里没有的就去请求5000里的数据

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class App extends Component {
+
+
+  getStudentData = ()=>{
+     axios.get('http://localhost:3000/students').then(
+        response=>{console.log('succcess',response.data);},
+        error=>{console.log('失败了',error);}
+     )
+  }
   render() {
     return (
       <div>
-          
+          <button onClick={this.getStudentData}>点我获取学生数据</button>
       </div>
     )
   }

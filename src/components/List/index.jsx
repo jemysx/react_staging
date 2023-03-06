2c80f33a645711782e9658a853ab099c01572c34
Ken <flh070147000303@gmail.com>
2023-03-06 18:06:58 +0900

    github案例的 组件拆分 和样式拆分

---
+++ src/components/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import "./index.css"
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank">
+            <img alt="avatar" src="https://picx.zhimg.com/v2-a9c966774c2d1f924a3ffe3fb85db199_720w.jpg?source=172ae18b" style={{width:'100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank">
+            <img alt="avatar" src="https://picx.zhimg.com/v2-a9c966774c2d1f924a3ffe3fb85db199_720w.jpg?source=172ae18b" style={{width:'100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank">
+            <img alt="avatar" src="https://picx.zhimg.com/v2-a9c966774c2d1f924a3ffe3fb85db199_720w.jpg?source=172ae18b" style={{width:'100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank">
+            <img alt="avatar" src="https://picx.zhimg.com/v2-a9c966774c2d1f924a3ffe3fb85db199_720w.jpg?source=172ae18b" style={{width:'100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank">
+            <img  alt="avatar" src="https://picx.zhimg.com/v2-a9c966774c2d1f924a3ffe3fb85db199_720w.jpg?source=172ae18b" style={{width:'100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+    </div>
+    )
+  }
+}

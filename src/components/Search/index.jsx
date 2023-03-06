eb67751a56b56390023c3526b9b15b567b814feb
Ken <flh070147000303@gmail.com>
2023-03-06 19:46:40 +0900

    1.连续解构赋值＋重命名 2.配置get请求的代理

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Search extends Component {
+
+  //获取用户的输入,
+  search = ()=>{
+    //常规解构赋值
+    // const {value} = this.keyWordElement
+   //连续解构赋值 + 重命名
+    // const {keyWordElement} = this
+    // keyWordElement.value
+    const {keyWordElement:{value:keyWord}} = this
+    console.log(keyWord);
+      //发送网络请求
+
+    axios.get(`/api1/search/users2?q=${keyWord}`).then(
+      response =>{console.log('成功了',response.data)},
+      error =>{console.log('失败了',error)}
+    )
+  }
+
+  
   render() {
     return (
       <div>
           <section className="jumbotron">
             <h3 className="jumbotron-heading">Search Github Users</h3>
             <div>
-              <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+              <input ref={(c)=>{this.keyWordElement= c}}  type="text" placeholder="enter the name you search"/>&nbsp;<button onClick={this.search}>Search</button>
             </div>
           </section>
       </div>

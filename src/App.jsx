1d7cbdd9f85cdab57f779551ac6c6f2369f807db
Ken <flh070147000303@gmail.com>
2023-03-09 17:20:41 +0900

    封装Navlink, 标签体内容也是一个特殊的标签属性:children

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { NavLink,Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import MyNavLink from './components/MyNavLink'
 export default class App extends Component {
 
   render() {
@@ -21,8 +22,9 @@ export default class App extends Component {
                 <a class="list-group-item active" href="./about.html">About</a>
                 <a class="list-group-item" href="./home.html">Home</a> */}
                 {/* 在react中靠路由链接实现切换组件 编写路由链接 */}
-                  <NavLink activeClassName='testCo' className="list-group-item" to="/about">About</NavLink>
-                  <NavLink activeClassName='testCo'  className="list-group-item" to="/home">Home</NavLink>
+
+                  <MyNavLink to="/about/">about</MyNavLink>
+                  <MyNavLink to="/home/" >home</MyNavLink>
               </div>
             </div>
             <div className="col-xs-6">

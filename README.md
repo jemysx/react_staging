7032aeeff5b14c3bd6fba2530e5de75b1d622f8c
Ken <flh070147000303@gmail.com>
2023-03-09 15:32:47 +0900

    路由的基本使用功能完成

--- README.md
+++ README.md
@@ -40,3 +40,15 @@
         console.log('请求出错',error)
         PubSub.publish('ken',{isLoading:false,err:error.message})
     }``   
+
+
+## 三， 路由的基本使用 
+
+   1.明确界面中的导航区,展示区
+
+   2.导航区的a标签改为Link标签
+
+   3.展示区写Route标签进行路径的匹配
+       <Route path='/xxxx' component ={Demo}/>
+   
+   4.<App>的最外侧包裹了一个<BrowserRouter>或者<HashRouter>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import React  from "react";
 //引入app组件
 import App from './App';
 import ReactDOM from 'react-dom/client'
-
+import { BrowserRouter } from "react-router-dom";
 //渲染app组件到页面
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container);
 root.render(
-    <App />
+    <BrowserRouter>
+            <App />
+    </BrowserRouter>
 );
